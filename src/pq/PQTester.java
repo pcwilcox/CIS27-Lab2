c6fbd4096dfe50f93892717d4408cfe88fc2d0ed
pcwilcox <petercwilcox@gmail.com>
2016-03-20 22:10:14 -0700

    pq's done

--- src/pq/PQTester.java
+++ src/pq/PQTester.java
@@ -1,5 +1,6 @@
 package pq;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -8,13 +9,15 @@ import java.util.Random;
  */
 public class PQTester
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Random rand = new Random();
 
         System.out.println("Program to test priority queue systems.");
         System.out.println("100 random jobs: ");
         int[] jobs = new int[100];
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             int j = rand.nextInt(101);
             jobs[i] = j;
             System.out.print(j + " ");
@@ -22,13 +25,15 @@ public class PQTester
         System.out.println();
         System.out.println("First test - First-In-First-Out queue:");
         Queue<Integer> jobQueue = new Queue<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             jobQueue.enqueue(jobs[i]);
         }
 
-        int lapsed = 0;
+        int lapsed   = 0;
         int jobCount = 0;
-        while (!jobQueue.isEmpty()) {
+        while (!jobQueue.isEmpty())
+        {
             int i = jobQueue.dequeue();
             System.out.println("Job #" + ++jobCount + ": " + " Waiting time: " + lapsed + " Processing Time: " + i);
             lapsed += i;
@@ -37,14 +42,41 @@ public class PQTester
         jobCount = 0;
 
         MinHeap jobHeap = new MinHeap();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             jobHeap.insert(jobs[i]);
         }
         System.out.println("Second Test - Shortest-Job-First:");
-        while (!jobHeap.isEmpty()) {
+        while (!jobHeap.isEmpty())
+        {
             int i = jobHeap.remove();
             System.out.println("Job #" + ++jobCount + ": " + " Waiting time: " + lapsed + " Processing Time: " + i);
             lapsed += i;
+        }
+
+        lapsed = 0;
+        jobCount = 0;
+        System.out.println("Third Test - Round Robin:");
+        ArrayList<Integer> jobList = new ArrayList<>();
+        for (int i = 0; i < 100; i++)
+        {
+            jobList.add(jobs[i]);
+        }
+
+        while (!jobList.isEmpty())
+        {
+            int i = jobList.remove(0);
+            System.out.println("Job #" + ++jobCount + ": " + " Waiting time: " + lapsed + " Processing Time: " + i);
+            lapsed += i;
+            i -= 20;
+            if (jobCount >= 100)
+            {
+                jobCount = 0;
+            }
+            if (i > 0)
+            {
+                jobList.add(i);
+            }
         }
     }
 }
